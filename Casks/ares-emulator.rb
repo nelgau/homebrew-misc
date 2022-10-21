6a1462996968cc16fae3f3b8a265c3807ff84c98
Nelson Gauthier <nelson.gauthier@gmail.com>
2022-10-21 13:05:19 -0700

    Add ares-emulator cask

---
+++ Casks/ares-emulator.rb
@@ -0,0 +1,20 @@
+cask "ares-emulator" do
+  version "130.1"
+  sha256 "34f80476cdc6acca4718e0af14171ab90193f6c5269d10b8443edf3d2f0f7558"
+
+  url "https://github.com/ares-emulator/ares/releases/download/v#{version}/ares-macos.zip",
+      verified: "https://github.com/ares-emulator/ares"
+  name "Ares Multi System Emulator"
+  desc "Cross-platform, multi-system emulator, focusing on accuracy and preservation"
+  homepage "https://ares-emu.net/"
+
+  auto_updates true
+  depends_on macos: ">= :el_capitan"
+
+  app "ares-v#{version}/ares.app"
+
+  zap trash: [
+    "~/Library/Application Support/ares",
+    "~/Library/Saved Application State/dev.ares.ares.savedState",
+  ]
+end
